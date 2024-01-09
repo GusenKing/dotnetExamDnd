7057774fc4a6c17344a33b671b1b7c279be97874
Emin <emin.sarux2004@yandex.ru>
2024-01-09 12:04:28 +0300

    db configured and monsters added to table via migrations

---
+++ Server/DB/AppDbContext.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Server.DB;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Monster> Monsters { get; set; } = null;
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var monsterListForDb = new List<Monster>()
+        {
+            new Monster
+            {
+                MonsterId = 1,
+                Name = "Troll",
+                HitPoints = 84,
+                AttackModifier = 40,
+                AttackPerRound = 1,
+                Damage = "8d10",
+                DamageModifier = 4,
+                ArmorClass = 15
+            },
+            new Monster
+            {
+                MonsterId = 2,
+                Name = "Bronze Dragon Wyrmling",
+                HitPoints = 32,
+                AttackModifier = 10,
+                AttackPerRound = 1,
+                Damage = "5d8",
+                DamageModifier = 3,
+                ArmorClass = 17
+            },
+            new Monster
+            {
+                MonsterId = 3,
+                Name = "Salamander",
+                HitPoints = 90,
+                AttackModifier = 24,
+                AttackPerRound = 1,
+                Damage = "12d10",
+                DamageModifier = 4,
+                ArmorClass = 15
+            },
+            new Monster
+            {
+                MonsterId = 4,
+                Name = "Brown Bear",
+                HitPoints = 34,
+                AttackModifier = 12,
+                AttackPerRound = 1,
+                Damage = "4d10",
+                DamageModifier = 4,
+                ArmorClass = 11
+            },
+            new Monster
+            {
+                MonsterId = 5,
+                Name = "Cat",
+                HitPoints = 12,
+                AttackModifier = 1,
+                AttackPerRound = 1,
+                Damage = "1d4",
+                DamageModifier = 1,
+                ArmorClass = 12
+            },
+        };
+        modelBuilder.Entity<Monster>().HasData(monsterListForDb);
+    }
+}
---
+++ Server/Migrations/20240109085700_AddMonstersToTable.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMonstersToTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Monsters",
+                columns: new[] { "MonsterId", "ArmorClass", "AttackModifier", "AttackPerRound", "Damage", "DamageModifier", "HitPoints", "Name" },
+                values: new object[,]
+                {
+                    { 1, 15, 40, 1, "8d10", 4, 84, "Troll" },
+                    { 2, 17, 10, 1, "5d8", 3, 32, "Bronze Dragon Wyrmling" },
+                    { 3, 15, 24, 1, "12d10", 4, 90, "Salamander" },
+                    { 4, 11, 12, 1, "4d10", 4, 34, "Brown Bear" },
+                    { 5, 12, 1, 1, "1d4", 1, 12, "Cat" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Monsters",
+                keyColumn: "MonsterId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Monsters",
+                keyColumn: "MonsterId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Monsters",
+                keyColumn: "MonsterId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Monsters",
+                keyColumn: "MonsterId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Monsters",
+                keyColumn: "MonsterId",
+                keyValue: 5);
+        }
+    }
+}
